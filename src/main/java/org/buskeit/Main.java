1d9be7e9c43f42c922e25076e3a60402fddf79bf
Benjamin Buske <info@buske-it.com>
2026-06-08 11:31:36 -0400

    Changed language to english.

--- src/main/java/org/buskeit/Main.java
+++ src/main/java/org/buskeit/Main.java
@@ -6,55 +6,55 @@ import java.util.Scanner;
 public class Main {
     static void main() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("\n##### BIENVENIDO AL BANCO BANESCO #####");
-        System.out.print("Favor ingresar su nombre: ");
+        System.out.println("\n##### WELCOME TO THE BANKING APP #####");
+        System.out.print("Please enter your name: ");
         String username =  scanner.nextLine();
 
         double balance = 1000.00;
         int choice;
 
         do {
-            System.out.println("##### MENU PRINCIPAL #####");
-            System.out.println("Bienvenido, " + username + "!");
-            System.out.println("1. Revisar Saldo");
-            System.out.println("2. Deposito");
-            System.out.println("3. Retiro");
+            System.out.println("##### MAIN MENU #####");
+            System.out.println("Welcome, " + username + "!");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
             System.out.println("4. Salir");
-            System.out.print("Por favor seleccione: ");
+            System.out.print("Please make your choice (1-4): ");
 
             choice = scanner.nextInt();
             switch (choice) {
                 case 1:
-                    System.out.printf("Su saldo actual es: $%.2f%n", balance);
+                    System.out.printf("Your current balance is: $%.2f%n", balance);
                     break;
                 case 2:
-                    System.out.print("Ingresa el monto a depositar: ");
+                    System.out.print("Amount to deposit: ");
                     double deposit = scanner.nextDouble();
 
                     if (deposit > 0) {
                         balance += deposit;
-                        System.out.printf("Deposito de $%.2f exitoso. Su nuevo saldo es $%.2f%n!", deposit, balance);
+                        System.out.printf("Deposit of $%.2f successful. Your new balance is $%.2f%n!", deposit, balance);
                     } else {
-                        System.out.println("Monto invalido. Debe ser un monto positivo.");
+                        System.out.println("The amount entered is invalid. It must be positive.");
                     }
                     break;
                 case 3:
-                    System.out.print("Ingresa el monto a retirar: ");
+                    System.out.print("Amount to withdraw: ");
                     double withdraw = scanner.nextDouble();
                     if (withdraw <= 0) {
-                        System.out.println("Monto invalido. Debe ser un monto positivo.");
+                        System.out.println("The amount entered is invalid. It must be positive.");
                     } else if (withdraw <=balance) {
                         balance -= withdraw;
-                        System.out.printf("Retiro exitoso. Su nuevo saldo es: $%.2f.%n", balance);
+                        System.out.printf("Withdrawal successful. Your new balance is: $%.2f.%n", balance);
                     } else {
-                        System.out.println("Codigo 51: Fondo insuficiente!");
+                        System.out.println("Balance Insufficient! Please try again.");
                     }
                     break;
                 case 4:
-                    System.out.println("Gracias por utilizar la app de Banco Banesco, " + username + "! Hasta pronto.");
+                    System.out.println("Thank you for using our banking app, " + username + "! See you soon.");
                     break;
                 default:
-                    System.out.println("Opción Invalida. Por favor intente nuevamente.");
+                    System.out.println("Invalid option, please try again.");
             }
         } while (choice != 4);
         scanner.close();
